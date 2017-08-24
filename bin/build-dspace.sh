10268f47c2c2df81e1f420ed97c186717704e2aa
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2017-08-24 13:25:47 -0700

    Task 9785: updated build-dspace.sh script to include overly-softlink.sh

--- bin/build-dspace.sh
+++ bin/build-dspace.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-DSPACE_SRC=/opt/tomcat/dspace/src
+SHELL_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DSPACE_HOME_DIR="$( cd $SHELL_SCRIPT_DIR/../.. && pwd )"
+DSPACE_SRC="$DSPACE_HOME_DIR/src"
+
+# Pickup latest overlays
+cd $DSPACE_HOME_DIR; campusrepo/bin/overlay-softlinks.sh campusrepo src; cd -
 
 # Build dspace:
-cd $DSPACE_SRC && mvn package
+cd $DSPACE_SRC; mvn package; cd -
 
 # Install dspace:
-cd $DSPACE_SRC/dspace/target/dspace-installer && ant fresh_install
+cd $DSPACE_SRC/dspace/target/dspace-installer; ant fresh_install; cd -
