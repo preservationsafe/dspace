e98ed708118f9ee1146c4ae74f8f8bdc04220b88
glbrimhall <glbrimhall@email.arizona.edu>
2017-08-24 20:56:39 +0000

    Task #9785: fixed bug in build-dspace.sh where it wasn't building within docker image

--- bin/build-dspace.sh
+++ bin/build-dspace.sh
@@ -1,14 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-SHELL_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DSPACE_HOME_DIR="$( cd $SHELL_SCRIPT_DIR/../.. && pwd )"
+if [ "$HOME" == "/opt/tomcat/dspace" ]; then
+  DSPACE_HOME_DIR="$HOME"
+else
+  SHELL_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  DSPACE_HOME_DIR="$( cd $SHELL_SCRIPT_DIR/../.. && pwd )"
+fi
+
 DSPACE_SRC="$DSPACE_HOME_DIR/src"
 
 # Pickup latest overlays
-cd $DSPACE_HOME_DIR; campusrepo/bin/overlay-softlinks.sh campusrepo src; cd -
+cd $DSPACE_HOME_DIR && campusrepo/bin/overlay-softlink.sh campusrepo src
 
 # Build dspace:
-cd $DSPACE_SRC; mvn package; cd -
+cd $DSPACE_SRC && mvn package
 
 # Install dspace:
-cd $DSPACE_SRC/dspace/target/dspace-installer; ant fresh_install; cd -
+cd $DSPACE_SRC/dspace/target/dspace-installer && ant fresh_install
