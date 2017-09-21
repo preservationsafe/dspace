73abad2046c22c6e9b1d918a391de020cea34ec2
Andy Osborne <cao89@email.arizona.edu>
2017-09-21 13:45:38 -0700

    Add a 'clean' option to build-dpsace.sh refs #9873

--- bin/build-dspace.sh
+++ bin/build-dspace.sh
@@ -11,17 +11,30 @@ else
 fi
 
 DSPACE_SRC="$DSPACE_HOME_DIR/src"
+DSPACE_RUN="$DSPACE_HOME_DIR/run"
+DSPACE_SRC_RELEASE="dspace-6.2-src-release"
 
-# Pickup latest overlays
-cd $DSPACE_HOME_DIR && bin/overlay-softlink.sh overlay src
+if [ $# -eq 1 ]; then
+  if [ $1 = "clean" ]; then
+    rm -rf $DSPACE_SRC/* && rm -rf $DSPACE_RUN/*
+    cd $DSPACE_HOME_DIR && ssh vitae "cat /data1/vitae/repos/$DSPACE_SRC_RELEASE.tar.gz" | pv | tar -xzf - \
+      && mv $DSPACE_SRC_RELEASE/* src && rmdir $DSPACE_SRC_RELEASE
+    bin/fix-permissions.sh
+    bin/overlay-softlink.sh overlay src
+    ln -fs $DSPACE_SRC /opt/tomcat/dspace
+  fi
+else
+  # Pickup latest overlays
+  cd $DSPACE_HOME_DIR && bin/overlay-softlink.sh overlay src
 
-# Default to a dev build
-if [ ! -f "$DSPACE_SRC/dspace/config/local.cfg" ]; then
-    cp "$DSPACE_SRC/dspace/config/local.cfg-dev" "$DSPACE_SRC/dspace/config/local.cfg"
+  # Default to a dev build
+  if [ ! -f "$DSPACE_SRC/dspace/config/local.cfg" ]; then
+      cp "$DSPACE_SRC/dspace/config/local.cfg-dev" "$DSPACE_SRC/dspace/config/local.cfg"
+  fi
+
+  # Build dspace:
+  cd $DSPACE_SRC && mvn package
+
+  # Install dspace:
+  cd $DSPACE_SRC/dspace/target/dspace-installer && ant fresh_install
 fi
-
-# Build dspace:
-cd $DSPACE_SRC && mvn package
-
-# Install dspace:
-cd $DSPACE_SRC/dspace/target/dspace-installer && ant fresh_install
