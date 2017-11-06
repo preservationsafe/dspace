39f4c29829a57b57c32c8848c7fae1289cf943fd
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2017-11-06 09:59:04 -0700

    dspace97: small refactor of code into functions to allow more complexity

--- bin/dspace-db-extract.pl
+++ bin/dspace-db-extract.pl
@@ -9,6 +9,8 @@ my $db_host    = shift @ARGV;
 my $db_admin   = shift @ARGV;
 my $db_pswd    = shift @ARGV;
 my $db_dspace  = shift @ARGV;
+my $dbh        = undef;
+my $dsn        = undef;
 my $NULL       = '_NULL_';
 
 if ( ! $db_host ) {
@@ -27,34 +29,58 @@ if ( ! $db_dspace ) {
   $db_dspace = 'dspacedev';
 }
 
-my $driver   = "Pg";
-my $database = $db_dspace;
-my $dsn      = "DBI:$driver:dbname = $database;host = $db_host;port = 5432";
-my $userid   = $db_admin;
-my $password = $db_pswd;
-my $dbh      = DBI->connect($dsn, $userid, $password, { RaiseError => 1 })
-   or die $DBI::errstr;
-print "Opened database successfully\n";
+sub open_db {
+  my $database = $db_dspace;
+  my $userid   = $db_admin;
+  my $password = $db_pswd;
 
-my $stmt = qq(SELECT * FROM metadatafieldregistry;);
-my $sth = $dbh->prepare( $stmt );
-my $rv = $sth->execute() or die $DBI::errstr;
-if($rv < 0) {
-print $DBI::errstr;
+  # Connection string for postgres:
+  my $driver   = "Pg";
+
+  $dsn = "DBI:$driver:dbname = $database;host = $db_host;port = 5432";
+
+  $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 })
+     or die $DBI::errstr;
+
+  print "DBACCESS: Opened $dsn successfully\n";
 }
 
-my $names = $sth->{NAME};
-my $types = $sth->{TYPE};
-print "SELECT:  ".$stmt."\n";
-print "COLUMNS: ".join( ', ', @$names )."\n";
-print "TYPES:   ".join( ', ', @$types )."\n";
-my $rownum = 1;
-my $row;
-
-while( defined( $row = $sth->fetchrow_arrayref() ) ) {
-  printf( 'ROW%-6d', $rownum );
-  print( join( ', ', map { defined ? $_ : $NULL } @$row )."\n" );
-  $rownum++;
+sub close_db {
+  print "DBACCESS: Closed $dsn successfully\n";
+  $dbh->disconnect();
+  undef $dbh;
 }
-print "Operation done successfully\n";
-$dbh->disconnect();
+
+sub extract_table {
+  my ( $table ) = @_;
+  
+  my $stmt = qq(SELECT * FROM $table;);
+  my $sth = $dbh->prepare( $stmt );
+  my $rv = $sth->execute() or die $DBI::errstr;
+  if($rv < 0) {
+  print $DBI::errstr;
+  }
+  
+  my @names = map { lc } @{$sth->{NAME}};
+  my $types = $sth->{TYPE};
+  print "SELECT:  ".$stmt."\n";
+  print "COLUMNS: ".join( ', ', @names )."\n";
+  print "TYPES:   ".join( ', ', @$types )."\n";
+  my $rownum = 1;
+  my $row;
+  
+  while( defined( $row = $sth->fetchrow_arrayref() ) ) {
+    printf( 'ROW%-6d', $rownum );
+    print( join( ', ', map { defined ? $_ : $NULL } @$row )."\n" );
+    $rownum++;
+  }
+
+  $sth->finish();
+  undef $sth;
+}
+
+&open_db();
+
+&extract_table( 'metadatafieldregistry' );
+
+&close_db();
