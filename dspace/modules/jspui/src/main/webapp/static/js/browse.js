580655fc44e1059fcab84114afef7ee9536ea797
Jeff Turman <turmanator@email.arizona.edu>
2017-12-11 16:20:56 -0700

    refs dspace128. add datepicker to browse.js

--- dspace/modules/jspui/src/main/webapp/static/js/browse.js
+++ dspace/modules/jspui/src/main/webapp/static/js/browse.js
@@ -18,5 +18,8 @@ $.noConflict();
     $(document).ready(function () {
         setSelected(document.getElementById("issue_year"));
         setSelected(document.getElementById("issue_month"));
+
+        $("#starts-date").datepicker({dateFormat: "yy-mm-dd" , monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"] ,dayNamesMin:["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]});
+        //jQuery("#ends-date").datepicker({dateFormat: "yy-mm-dd", monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"], dayNamesMin:["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]});
     });
 })(jQuery);
